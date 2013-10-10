8ba60eb389ea2def1863ea14efb3ff0c39e4b72b
HolyCow <mike.beasterfeld@gmail.com>
2013-10-10 15:11:07 -0500

    add show_id to title table

--- app/models/title.rb
+++ app/models/title.rb
@@ -1,3 +1,9 @@
 class Title < ActiveRecord::Base
-  attr_accessible :created_at, :ircuser_id, :title, :title_lc
+  attr_accessible :created_at, :irc_user_id, :show_id, :title, :title_lc
+
+  validates :content, :length => { :maximum => 50 }
+
+  belongs_to :show
+
+  has_one :irc_user
 end
