85e283315102bd69757ea43ce1c9aea752ee3ea0
HolyCow <mike.beasterfeld@gmail.com>
2013-10-21 01:00:34 -0500

    fix typo, always run tests

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
   	current_show = Show.current_show
   	if current_show
-  		@current_show_title = current_show.current
+  		@current_show_title = current_show.title
   	else
   		@current_show_title = "No shows yet"
   	end
