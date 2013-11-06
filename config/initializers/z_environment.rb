3dff8f732a83d9fab6425ad0456c40ce5b6364b3
HolyCow <mike.beasterfeld@gmail.com>
2013-11-05 20:55:57 -0600

    Use environment variables if they are defined

---
+++ config/initializers/z_environment.rb
@@ -0,0 +1,5 @@
+
+Settings.twitter_consumer_key = ENV['TWITTER_CONSUMER_KEY'] if ENV['TWITTER_CONSUMER_KEY']
+Settings.twitter_consumer_secret = ENV['TWITTER_CONSUMER_SECRET'] if ENV['TWITTER_CONSUMER_SECRET']
+Settings.live_url = ENV['LIVE_URL'] if ENV['LIVE_URL']
+
