7881a7595192401416983defabb89a65fb502112
HolyCow <mike.beasterfeld@gmail.com>
2013-11-05 22:56:57 -0600

    enable use of environment variables to override rails config

--- config/initializers/z_environment.rb
+++ config/initializers/z_environment.rb
@@ -3,3 +3,4 @@ Settings.twitter_consumer_key = ENV['TWITTER_CONSUMER_KEY'] if ENV['TWITTER_CONS
 Settings.twitter_consumer_secret = ENV['TWITTER_CONSUMER_SECRET'] if ENV['TWITTER_CONSUMER_SECRET']
 Settings.live_url = ENV['LIVE_URL'] if ENV['LIVE_URL']
 
+puts Settings
