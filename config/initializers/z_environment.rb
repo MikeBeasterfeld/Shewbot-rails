5ff2873990272bbd4ca51b5cc3ae38ff97e793f6
HolyCow <mike.beasterfeld@gmail.com>
2014-01-17 09:53:49 -0600

    option to hide twitter auth

--- config/initializers/z_environment.rb
+++ config/initializers/z_environment.rb
@@ -2,3 +2,4 @@
 Settings.twitter_consumer_key = ENV['TWITTER_CONSUMER_KEY'] if ENV['TWITTER_CONSUMER_KEY']
 Settings.twitter_consumer_secret = ENV['TWITTER_CONSUMER_SECRET'] if ENV['TWITTER_CONSUMER_SECRET']
 Settings.live_url = ENV['LIVE_URL'] if ENV['LIVE_URL']
+Settings.show_twitter_auth = ENV['SHOW_TWITTER_AUTH'] if ENV['SHOW_TWITTER_AUTH']
