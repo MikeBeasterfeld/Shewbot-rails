eb70171b533874e0ceaf5163dfca3c107684bc2f
HolyCow <mike.beasterfeld@gmail.com>
2014-01-28 20:32:10 -0600

    update config so environment variables persist

--- config/initializers/z_environment.rb
+++ config/initializers/z_environment.rb
@@ -1,5 +1,14 @@
 
-Settings.twitter_consumer_key = ENV['TWITTER_CONSUMER_KEY'] if ENV['TWITTER_CONSUMER_KEY']
-Settings.twitter_consumer_secret = ENV['TWITTER_CONSUMER_SECRET'] if ENV['TWITTER_CONSUMER_SECRET']
-Settings.live_url = ENV['LIVE_URL'] if ENV['LIVE_URL']
-Settings.show_twitter_auth = ENV['SHOW_TWITTER_AUTH'] if ENV['SHOW_TWITTER_AUTH']
+environment_settings = {};
+
+environment_settings[:twitter_consumer_key] = ENV['TWITTER_CONSUMER_KEY'] if ENV['TWITTER_CONSUMER_KEY']
+environment_settings[:twitter_consumer_secret] = ENV['TWITTER_CONSUMER_SECRET'] if ENV['TWITTER_CONSUMER_SECRET']
+environment_settings[:live_url] = ENV['LIVE_URL'] if ENV['LIVE_URL']
+environment_settings[:show_twitter_auth] = ENV['SHOW_TWITTER_AUTH'] if ENV['SHOW_TWITTER_AUTH']
+
+File.open(Dir.pwd + '/environment.yaml', 'w+') do |f| 
+	f.write(environment_settings.to_yaml)
+end
+
+Settings.add_source!(Dir.pwd + '/environment.yaml');
+Settings.reload!
