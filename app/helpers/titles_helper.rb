9148d0c2e436db1654f90877d2d6cad1de57d880
HolyCow <mike.beasterfeld@gmail.com>
2015-01-07 17:28:16 -0600

    check for 'slug' attribute in in live json to get show title, but default to 'title'

--- app/helpers/titles_helper.rb
+++ app/helpers/titles_helper.rb
@@ -9,6 +9,7 @@ module TitlesHelper
         live_hash = JSON.parse(RestClient.get Settings.live_url)
         if live_hash and live_hash.has_key?("live") and live_hash["live"]
           broadcast = live_hash["broadcast"] if live_hash.has_key? "broadcast"
+          title = broadcast["slug"] if broadcast.has_key? "slug"
           title = broadcast["title"] if broadcast.has_key? "title"
         end
       rescue
